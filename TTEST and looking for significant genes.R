b460691ab63d8cf61be2b459952132c2d30a2188
Lyn16 <32976783+Lyn16@users.noreply.github.com>
2018-11-27 13:11:54 -0800

    Update TTEST and looking for significant genes.R

--- TTEST and looking for significant genes.R
+++ TTEST and looking for significant genes.R
@@ -4,21 +4,17 @@ setwd("/Users/lyn/Final_Project/Step3. Data analysis")
 genes1<-read.csv("/Users/lyn/Final_Project/TCGA_AML_mRNA_data_for_analysis.csv", header = TRUE, sep = ",", quote = "\"", dec = ".", fill = TRUE, row.names = 2)
 genes1<-genes1[,-1]
 genes1_trans<-t(genes1)
-#corr<-cor(genes1_trans)
-#melted_corr <- melt(corr)
-#head(melted_corr)
 
 APOC2<-genes1["ENSG00000234906",]
 genes1["APOC2",]<-APOC2
 quantile(APOC2)
 highexpression<-which(APOC2 > 0.4639374)
-#highexpression_patients<-colnames(genes1[highexpression])
 lowexpression<-which(APOC2 <= 0.05992213 )
 mediumexpression<-which(APOC2<=0.4639374 & APOC2>0.05992213)
-genes1<-genes1[-54714,]
+
+genes1<-genes1[-54714,]#exclude the additional apoc2 colunm#
 
 # for TTest#
-#highexpression_patients<-colnames(genes1[highexpression])
 highexpression_patients<-genes1[highexpression]
 lowexpression_patients<-genes1[lowexpression]
 lowexpression_patients_transpose<-t(lowexpression_patients)
@@ -32,10 +28,12 @@ for (i in 1:54713){
 }
 result
 
+#save the TTEST result#
 write.csv(result, file ="/Users/lyn/Final_Project/TTEST_result.csv")
 genesselected<-read.csv("/Users/lyn/Final_Project/TTEST_result.csv", header = TRUE, sep = ",", quote = "\"", dec = ".", fill = TRUE, row.names = 1)
 rownames(genesselected)<-rownames(genes1)
 colnames(genesselected)<-c("statistic_value","p_value")
+
 #looking for genes that p-value<0.05#
 p_value<-genesselected[,2]
 significantgenes<-which(p_value<0.05)
@@ -43,5 +41,6 @@ significantgenes_id<-rownames(genesselected[significantgenes,])
 tableforpathwayanalysis<-genesselected[significantgenes_id,]
 tableforpathwayanalysis1<-tableforpathwayanalysis[order(tableforpathwayanalysis[,2]),]
 
+#select the top 100 and put into IPA#
 write.csv(tableforpathwayanalysis1, file = "/Users/lyn/Final_Project/TTEST_result_significantgenes.csv")
 write(significantgenes_id, file ="/Users/lyn/Final_Project/significantgenes_id.txt")
